6a3a7e8b37bb9f31fdb0b4344b6621ab5afb30e7
RodAnd19 <00078421@uca.edu.sv>
2023-05-20 21:21:33 -0600

    Laboratorio 08 terminado lo que me falto (Practicando para el parcial) :D

--- Lab 08/app/src/main/java/com/example/laboratorio05/ui/movie/MovieFragment.kt
+++ Lab 08/app/src/main/java/com/example/laboratorio05/ui/movie/MovieFragment.kt
@@ -5,13 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.example.laboratorio05.data.movies
 import com.example.laboratorio05.databinding.FragmentMovieBinding
 import com.example.laboratorio05.ui.movie.viewmodel.MovieViewModel
 
 class MovieFragment : Fragment() {
 
     private lateinit var binding: FragmentMovieBinding
-    private lateinit var movieData: MovieViewModel
+
+    private val movieViewModel: MovieViewModel by activityViewModels {
+        MovieViewModel.Factory
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,11 +26,13 @@ class MovieFragment : Fragment() {
         return binding.root
     }
 
-    private fun setData() {
-        binding.textView7.text = movieData.name.toString()
-        binding.textView5.text = movieData.qualification.toString()
-        binding.textView6.text = movieData.category.toString()
-        binding.descriptionTextView.text = movieData.description.toString()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        setViewModel()
+    }
+
+    private fun setViewModel() {
+        binding.viewmodel = movieViewModel
     }
 }
